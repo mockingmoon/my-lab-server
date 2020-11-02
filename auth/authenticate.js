26ad9eca55fb2a990647b07f0a45f5108a0ff182
Purujit Srinivasan <ipurujit@outlook.com>
2020-11-02 20:33:13 +0530

    Initial setup

---
+++ auth/authenticate.js
@@ -0,0 +1,3 @@
+var jwt = require('jsonwebtoken');
+var ExtractJwt = require('passport-jwt').ExtractJwt;
+var JwtStrategy = require('passport-jwt').Strategy;
---
+++ auth/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "secretKey" : "12345-54321-67890-09876",
+    "mongoUrl": "mongodb://localhost:12017/myLab"
+};
